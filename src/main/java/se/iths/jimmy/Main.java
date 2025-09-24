88fe1ce52611e66e0c685a0455e525f2f0019247
user.name=Jimmy Grafström <212706765+Jimmy-Grafstrom@users.noreply.github.com>
2025-09-24 14:25:14 +0200

    First commit.

--- src/main/java/se/iths/jimmy/Main.java
+++ src/main/java/se/iths/jimmy/Main.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
         Webshop webshop = new Webshop();
 
         webshop.webshopCompose(scanner);
